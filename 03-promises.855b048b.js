7f19c2ea07b5b56e6a20cb67647a36d947e3d302
VitaliyaOvechko <113234981+VitaliyaOvechko@users.noreply.github.com>
2023-02-07 20:43:16 +0000

    Deploying to gh-pages from @ VitaliyaOvechko/goit-js-hw-09@df8357792bbf93a2ee3b38af4724074e11facffa 🚀

---
+++ 03-promises.855b048b.js
@@ -0,0 +1,2 @@
+!function(){var n=document.querySelector("form");console.log(n),n.addEventListener("submit",(function(n,o){Math.random()}))}();
+//# sourceMappingURL=03-promises.855b048b.js.map
---
+++ 03-promises.c1a5c2af.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("form");console.log(o),o.addEventListener("submit",(function(o,e){Math.random()}));
+//# sourceMappingURL=03-promises.c1a5c2af.js.map
