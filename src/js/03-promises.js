df8357792bbf93a2ee3b38af4724074e11facffa
Vitaliya Ovechko <ovechko.vita@gmail.com>
2023-02-07 22:42:50 +0200

    Додає стилізацію Завдань 1,2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+const form = document.querySelector('form');
+console.log(form);
+
+form.addEventListener('submit', createPromise);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +12,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
